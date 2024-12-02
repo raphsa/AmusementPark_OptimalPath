14d4ef1017dc75dcaea39b9826c05f1f9b7f3efa
raphsa <saliraffaele@gmail.com>
2024-12-02 23:17:33 +0100

    Defined the two methods to update open or close attraction

--- internal_update.py
+++ internal_update.py
@@ -1,7 +1,20 @@
 
 
 def attractions_closed():
-    pass
+    att_news = input("Has any attraction just closed?\n").strip().lower()
+    if att_news == "yes":
+        att_closed = input("Digit separating by comma the attraction/s that has closed\n").strip().lower().replace(""," ")
+        att_closed = att_closed.split(",")
+    attractions_spec.loc[attractions_spec["Attraction"].isin(att_closed), "IsItOpen"] = False
+    return attractions_spec
+
+def attraction_open():
+    att_news = input("Has any attraction just opened?\n").strip().lower()
+    if att_news == "yes":
+        att_opened = input("Digit separating by comma the attraction/s that has opened\n").strip().lower().replace(""," ")
+        att_opened = att_opened.split(",")
+    attractions_spec.loc[attractions_spec["Attraction"].isin(att_opened), "IsItOpen"] = True
+    return attractions_spec
 
 
 def queue_update():
